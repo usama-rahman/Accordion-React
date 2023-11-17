cc79438cf63b7ac7e7e42e45ad1fd90c5bc73682
Usama Rahman <rahmanusama390@gmail.com>
2023-11-17 11:09:41 +0600

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -26,20 +26,43 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          title={el.title}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          num={i}
+          key={el.title}
+        >
+          {el.text}{" "}
+        </AccordionItem>
       ))}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Test 1"
+        num={22}
+        key="test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusuable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
   function handelToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -47,7 +70,7 @@ function AccordionItem({ num, title, text }) {
       <p className="number"> {num < 9 ? `0${num + 1} ` : num + 1} </p>
       <p className="title"> {title} </p>
       <p className="icon"> {isOpen ? "-" : "+"} </p>
-      {isOpen && <div className="content-box"> {text} </div>}
+      {isOpen && <div className="content-box"> {children} </div>}
     </div>
   );
 }
